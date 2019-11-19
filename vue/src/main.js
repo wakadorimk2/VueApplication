edd3a1f28ed986659fba2bda47345562db7466e9
wakadorimk2 <moriyamakensetsu@yahoo.co.jp>
2019-11-19 22:28:06 +0900

    Add Vuetify

--- vue/src/main.js
+++ vue/src/main.js
@@ -2,10 +2,12 @@ import 'bootstrap/dist/css/bootstrap.css';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import vuetify from '@/plugins/vuetify';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  vuetify,
   render: h => h(App),
 }).$mount('#app');
