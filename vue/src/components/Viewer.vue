ea43f3cdf7bc252c6f942d878ecadc48e982d670
wakadorimk2 <moriyamakensetsu@yahoo.co.jp>
2019-11-21 21:38:45 +0900

    Add image tiling

--- vue/src/components/Viewer.vue
+++ vue/src/components/Viewer.vue
@@ -1,25 +1,66 @@
 <template>
-  <v-carousel hide-delimiters>
-    <v-carousel-item
-      v-for="(fav, i) in faves"
-      :key="i"
-      :src="fav.url"
-    >
-    </v-carousel-item>
-  </v-carousel>
+  <v-card
+    class="mx-auto"
+    :max-width="windowSize.x"
+  >
+    <v-container fluid fill-height>
+      <v-btn
+        fab
+        bottom
+        right
+        fixed
+        color=#f99
+      >
+        <v-icon color=#fff>mdi-heart</v-icon>
+      </v-btn>
+      <v-row dense>
+        <v-col
+          v-for="(fav, index) in faves"
+          :key="index"
+          :cols="fav.sizes[display]['h']"
+        >
+          <v-card flat tile>
+            <v-img
+              :src="fav.src"
+              :height="windowSize.y/5"
+              :width="fav.sizes[display]['w']/2"
+              aspect-ratio="1"
+            >
+            </v-img>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
 </template>
+
 
 <script>
 import axios from 'axios';
 
 export default {
   name: 'Viewer',
-  data() {
-    return {
-      faves: {},
-    };
+  data: () => ({
+    faves: {},
+    windowSize: {
+      x: 0,
+      y: 0,
+    },
+    display: 'small',
+
+  }),
+
+  created() {
+    this.getFaves();
   },
+  mounted() {
+    this.onResize();
+  },
+
   methods: {
+    onResize() {
+      this.windowSize = { x: window.innerWidth, y: window.innerHeight };
+    },
     getFaves() {
       const path = 'http://localhost:5000/faves';
       axios.get(path)
@@ -31,9 +72,6 @@ export default {
           console.error(error);
         });
     },
-  },
-  created() {
-    this.getFaves();
   },
 };
 </script>
